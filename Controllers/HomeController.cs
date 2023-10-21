fa4afad651f040233e60743654d238707200590e
Yuvin15 <yuvinpadayachee@gmail.com>
2023-10-21 17:01:34 +0200

    Noti's need to show now and @Uwais needs to check the DB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Http;
 using WIL_Project.Models;
+using Microsoft.Extensions.Logging;  // ensure this is imported
+using System.IO;   // for MemoryStream
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WIL_Project.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        CobraContext _obraContext = new CobraContext();
+
+        Ticket newTicket = new Ticket();
+        TicketAttachment newAttachment = new TicketAttachment();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
@@ -25,10 +38,10 @@ namespace WIL_Project.Controllers
 
         public IActionResult AllTickets()
         {
-            List<Ticket> tickets = GetTicketsFromDatabase(); // Assuming you have a method to fetch data from the database
+            List<Ticket> tickets = GetTicketsFromDatabase(); 
             return View(tickets);
         }
-        private List<Ticket> GetTicketsFromDatabase()
+        private List<Ticket> GetTicketsFromDatabase() // Change this to getting stuff from db
         {
             // Implement logic to fetch data from your database here
             // Example:
@@ -36,25 +49,25 @@ namespace WIL_Project.Controllers
             {
                 new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
                 new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
-                new Ticket { TicketId = 1, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
-                new Ticket { TicketId = 2, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" }
+                new Ticket { TicketId = 3, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 4, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
+                new Ticket { TicketId = 5, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 6, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
+                new Ticket { TicketId = 7, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 8, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
+                new Ticket { TicketId = 9, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 10, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
+                new Ticket { TicketId = 11, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 12, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" },
+                new Ticket { TicketId = 13, TicketSubject = "Sample Subject 1", TicketCreationDate = DateTime.Now, TicketStatus = "Open" },
+                new Ticket { TicketId = 14, TicketSubject = "Sample Subject 2", TicketCreationDate = DateTime.Now, TicketStatus = "Closed" }
             };
             return tickets;
         }
 
         private static void sendResponse()
         {
-
+            // This is the email they send back when responding to the ticket
         }
 
         public IActionResult YourTickets()
@@ -62,9 +75,65 @@ namespace WIL_Project.Controllers
             return View(); 
         }
 
+        [HttpGet]
         public IActionResult CreateTicket()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTicket(List<IFormFile> attachments, string subject, string body)
+        {
+            try
+            {
+                var newTicket = new Ticket
+                {
+                    TicketSubject = subject,
+                    TicketBody = body,
+                    TicketCreationDate = DateTime.Now
+                };
+                _obraContext.Tickets.Add(newTicket);
+                _obraContext.SaveChanges();
+
+                if (attachments != null && attachments.Any())
+                {
+                    var newAttachment = new TicketAttachment();
+
+                    if (attachments.Count > 0)
+                    {
+                        newAttachment.Attachments1 = GetBytesFromFormFile(attachments[0]);
+                    }
+                    if (attachments.Count > 1)
+                    {
+                        newAttachment.Attachments2 = GetBytesFromFormFile(attachments[1]);
+                    }
+                    if (attachments.Count > 2)
+                    {
+                        newAttachment.Attachments3 = GetBytesFromFormFile(attachments[2]);
+                    }
+
+                    _obraContext.TicketAttachments.Add(newAttachment);
+                    _obraContext.SaveChanges();
+                }
+
+                TempData["SuccessMessage"] = "Ticket submitted successfully!";
+                return RedirectToAction("YourTickets", "Home");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Error while submitting ticket. Please try again.";
+                return View(); 
+            }
+
+        }
+
+
+        private byte[] GetBytesFromFormFile(IFormFile formFile)
+        {
+            using var memoryStream = new MemoryStream();
+            formFile.CopyTo(memoryStream);
+            return memoryStream.ToArray();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Program.cs
+++ Program.cs
@@ -1,128 +1,33 @@
-using Microsoft.AspNetCore.Authentication.OAuth;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
- 
-
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 
-
-
-// Configure authentication
-
-builder.Services.AddAuthentication(options =>
-
-{
-
-    options.DefaultAuthenticateScheme = "Cookies";
-
-    options.DefaultChallengeScheme = "Microsoft";
-
-})
-
-.AddCookie("Cookies")
-
-.AddOAuth("Microsoft", options =>
-
-{
-
-    IConfiguration configuration = builder.Configuration;
-
-    options.ClientId = configuration["Authentication:Microsoft:ClientId"];
-
-    options.ClientSecret = configuration["Authentication:Microsoft:ClientSecret"];
-
-    options.CallbackPath = "/signin-microsoft"; // Customize this as needed
-
-    options.AuthorizationEndpoint = "https://login.microsoftonline.com/{tenant-id}/oauth2/v2.0/authorize"; // Replace {tenant-id} with the appropriate value
-
-    options.TokenEndpoint = "https://login.microsoftonline.com/{tenant-id}/oauth2/v2.0/token"; // Replace {tenant-id} with the appropriate value
-
-
-
-    options.ClaimActions.MapJsonKey("urn:microsoft:identity:username", "upn");
-
-
-
-    options.SaveTokens = true; // Save the access tokens for future use
-
-
-
-    options.Events = new OAuthEvents
-
-    {
-
-        OnCreatingTicket = context =>
-
-        {
-
-            // Handle additional logic when creating the ticket
-
-            // For example, you can map user claims here
-
-            return Task.CompletedTask;
-
-        }
-
-    };
-
-});
-
-
+// Add Session services.
+builder.Services.AddSession();
 
 var app = builder.Build();
 
-
-
 // Configure the HTTP request pipeline.
-
 if (!app.Environment.IsDevelopment())
-
 {
-
     app.UseExceptionHandler("/Home/Error");
-
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-
     app.UseHsts();
-
 }
 
-
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
-
+// Use Session middleware.
+app.UseSession();
 
 app.UseRouting();
 
-
-
-// Use authentication
-
-app.UseAuthentication();
-
 app.UseAuthorization();
 
-
-
 app.MapControllerRoute(
-
     name: "default",
-
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 
 app.Run();
